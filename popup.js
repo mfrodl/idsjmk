c7a3ff6900e484d3ab586f56c0e27c823a3668cf
Martin Frodl <maarilainen@gmail.com>
2018-05-26 23:52:51 +0200

    Do not reload popup, only redraw what is necessary

--- popup.js
+++ popup.js
@@ -40,6 +40,10 @@ var submitHandler = function(event) {
   // Suppress form submit
   event.preventDefault();
 
+  // Close autocomplete dropdowns
+  $('#from').autocomplete('close');
+  $('#to').autocomplete('close');
+
   var from = $('#from').val();
   var to = $('#to').val();
 
@@ -87,55 +91,55 @@ var submitHandler = function(event) {
       }
     })
   ).then(function() {
-    // Show error messages if stop has not been found
-    $('#fromError').html(fromFound ? '' : 'Zastávka nenalezena');
-    $('#toError').html(toFound ? '' : 'Zastávka nenalezena');
-
     if (fromFound && toFound) {
       // Both stops exist, update saved routes and reload
       var searchedRoute = new Route(from, to);
-
-      chrome.storage.local.get({'routes': []}, function(result) {
-        var routes = result.routes;
-        var routeFound = false;
-
-        // Check if the searched route is among the saved routes
-        for (var i = 0; i < routes.length; i++) {
-          if (searchedRoute.equals(routes[i])) {
-            routeFound = true;
-            searchedRoute = routes[i];
-            break;
-          }
-        }
-
-        // Searched route not found in saved routes, add it
-        if (!routeFound) {
-          routes.push(searchedRoute);
-        }
-
-        // Increment number of views for route
-        searchedRoute.views++;
-
-        // Save updated routes to storage
-        chrome.storage.local.set({
-          routes: routes,
-          defaultRoute: searchedRoute
-        });
-
-        // Reload popup
-        location.reload();
-      });
+      updateStatsAndReload(searchedRoute);
     }
+  });
+}
+
+/*
+ * Update search statistics in browser storage and reload popup
+ */
+var updateStatsAndReload = function(route) {
+  chrome.storage.local.get({'routes': []}, function(result) {
+    var routes = result.routes;
+    var routeFound = false;
+
+    // Check if the searched route is among the saved routes
+    for (var i = 0; i < routes.length; i++) {
+      if (route.equals(routes[i])) {
+        routeFound = true;
+        route = routes[i];
+        break;
+      }
+    }
+
+    // Searched route not found in saved routes, add it
+    if (!routeFound) {
+      routes.push(route);
+    }
+
+    // Increment number of views for route
+    route.views++;
+
+    // Save updated routes to storage
+    chrome.storage.local.set({
+      routes: routes,
+      defaultRoute: route
+    });
+
+    // Reload popup
+    redraw();
   });
 }
 
 /*
  * Find connection for given route and time and show them in popup window
  */
-var showConnections = function(from, to, datetime, lowdtr) {
+var showConnections = function(from, to, date, time, lowdtr) {
   // Construct full URL
-  var date = datetime.format('D.M.YY');
-  var time = datetime.format('H:mm');
   var query = {f: from, t: to, date: date, time: time, lowdtr: lowdtr};
   var url = 'https://www.idsjmk.cz/spojeni.aspx?' + $.param(query);
 
@@ -162,16 +166,18 @@ var showConnections = function(from, to, datetime, lowdtr) {
         return;
       }
 
+      var datetime = moment.tz(`${date} ${time}`, 'D.M. H:mm', 'Europe/Prague');
+
       var timeString = [lastDate, lastTime].join(' ');
 
       // Convert the time to moment in Europe/Prague timezone
-      var newTime = moment.tz(timeString, "D.M. H:mm", "Europe/Prague");
+      var newDateTime = moment.tz(timeString, "D.M. H:mm", "Europe/Prague");
       // Add 1 minute to avoid duplicate results
-      newTime.add(1, 'minutes');
+      newDateTime.add(1, 'minutes');
       // When the new time is less than the old one, we have probably hit turn
       // of year, so the year needs to be incremented
-      if (newTime < datetime) {
-        newTime.add(1, 'years');
+      if (newDateTime < datetime) {
+        newDateTime.add(1, 'years');
       }
 
       // Show link to find more connections
@@ -180,7 +186,9 @@ var showConnections = function(from, to, datetime, lowdtr) {
       );
       // Link handler
       $('#showMoreLink').click(function() {
-        showConnections(from, to, newTime, lowdtr);
+        var newDate = newDateTime.format('D.M.YY');
+        var newTime = newDateTime.format('H:mm');
+        showConnections(from, to, newDate, newTime, lowdtr);
         return false;
       });
 
@@ -191,33 +199,36 @@ var showConnections = function(from, to, datetime, lowdtr) {
   });
 }
 
-$(function() {
-  // Set autocomplete for input fields
-  $('#from').autocomplete({source: autocompleteSource});
-  $('#to').autocomplete({source: autocompleteSource});
-
-  $('#date').datepicker({dateFormat: 'd.m.y'});
-
-  // Show current date and time in input fields
-  datetime = moment().tz('Europe/Prague');
-  $('#time').val(datetime.format('H:mm'));
-  $('#date').val(datetime.format('D.M.YY'));
-
-  // Form submit handler
-  $('#searchForm').submit(submitHandler);
-
+var redraw = function() {
   // Load saved routes from local storage
   chrome.storage.local.get({'routes': [], 'defaultRoute': null, 'lowdtr': 0},
     function(result) {
       // Set opacity of wheelchair icon
       $('#wheelchair').css('opacity', result.lowdtr ? 1 : 0.5);
 
+      // Show default route in input fields and search for connection or show
+      // placeholder when no default route available
       var defaultRoute = result.defaultRoute;
-
-      // Show placeholder when no routes available
-      var showPlaceholder = true;
+      if (defaultRoute) {
+        $('#from').val(defaultRoute.from);
+        $('#to').val(defaultRoute.to);
+        $('#connections').empty();
+        $('#connections').removeClass('placeholder');
+        showConnections(
+          defaultRoute.from,
+          defaultRoute.to,
+          $('#date').val(),
+          $('#time').val(),
+          result.lowdtr
+        );
+      }
+      else {
+        $('#showMore').empty();
+        $('#connections').addClass('placeholder');
+      }
 
       // Show six routes with most views
+      $('#routes').empty();
       var routes = result.routes.sort((x, y) => x.views - y.views).slice(-6);
       for (let route of routes) {
         showPlaceholder = false;
@@ -239,57 +250,80 @@ $(function() {
           routeBox.addClass('defaultRoute');
           routeLink.removeClass('setDefaultRoute');
           routeLink.removeAttr('href');
-
-          // Show default route in input fields
-          $('#from').val(route.from);
-          $('#to').val(route.to);
-
-          showConnections(route.from, route.to, datetime, result.lowdtr);
         }
 
         $('#routes').append(routeBox);
         $('#routes').css('padding', '5px');
       }
 
-      if (showPlaceholder) {
-        $('#showMore').empty();
-        $('#connections').addClass('placeholder');
-      }
-      else {
-        $('#connections').removeClass('placeholder');
-      }
-
-      // Submit form on Enter
-      $('input').keypress(function(e) {
-        if (e.which == 13) {
-          $('#from').autocomplete('close');
-          $('#to').autocomplete('close');
-          $('#searchForm').submit();
-        }
-      });
-
       // Change default route and reload when a link is clicked
       $('a.setDefaultRoute').click(function() {
         var defaultRoute = new Route($(this).data('from'), $(this).data('to'));
         chrome.storage.local.set({defaultRoute: defaultRoute});
-        location.reload();
+        redraw();
       });
 
-      // From-to swap handler
-      $('#swapFromTo').click(function() {
-        var from = $('#from').val()
-        var to = $('#to').val();
-        $('#from').val(to);
-        $('#to').val(from);
-        $('#searchForm').submit();
-      });
-
-      // Low-floor switch handler
-      $('#wheelchair').click(function() {
-        chrome.storage.local.set({'lowdtr': 1 - result.lowdtr}, function() {
-          location.reload();
-        })
-      });
     }
   );
+}
+
+/*
+ * From-to swap handler
+ */
+var swapHandler = function() {
+  var newRoute = new Route($('#to').val(), $('#from').val());
+  chrome.storage.local.set({defaultRoute: newRoute});
+  updateStatsAndReload(newRoute);
+}
+
+/*
+ * Low-floor switch handler
+ */
+var wheelchairHandler = function() {
+  chrome.storage.local.get({'lowdtr': 0}, function(result) {
+    chrome.storage.local.set({'lowdtr': 1 - result.lowdtr}, function() {
+      $('#searchForm').submit();
+    })
+  });
+}
+
+$(function() {
+  // Set autocomplete for input fields
+  $('#from').autocomplete({
+    source: autocompleteSource,
+    select: function(event, ui) {
+      $('#from').val(ui.item.label);
+      $('#searchForm').submit();
+    }
+  });
+  $('#to').autocomplete({
+    source: autocompleteSource,
+    select: function(event, ui) {
+      $('#to').val(ui.item.label);
+      $('#searchForm').submit();
+    }
+  });
+
+  // Enable date picker widget
+  $('#date').datepicker({dateFormat: 'd.m.y'});
+
+  // Show current date and time in input fields
+  var datetime = moment().tz('Europe/Prague');
+  $('#time').val(datetime.format('H:mm'));
+  $('#date').val(datetime.format('D.M.YY'));
+
+  // Form submit handler
+  $('#searchForm').submit(submitHandler);
+
+  // From-to swap handler
+  $('#swapFromTo').click(swapHandler);
+
+  // Low-floor switch handler
+  $('#wheelchair').click(wheelchairHandler);
+
+  // Submit form whenever time or date is changed
+  $('#time').change(submitHandler);
+  $('#date').change(submitHandler);
+
+  redraw();
 });
